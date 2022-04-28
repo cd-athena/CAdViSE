95df3bbf0434a9b9027d5c917d338e08ba5f1f0b
Babak <tbabak@users.noreply.github.com>
2022-04-28 13:00:20 +0200

    Docker Build Automation & More (#17)
    * add github action yml files
    
    * update maintainer emails
    
    * update client init phase
    
    * add new dataset from itec server
    update server init phase

--- client/init.sh
+++ client/init.sh
@@ -8,8 +8,8 @@ sudo git clone https://github.com/cd-athena/wondershaper.git /home/ec2-user/wond
 config=$(cat /home/ec2-user/config.json)
 player=$(echo "$config" | jq -r '.player')
 
-sudo docker pull babakt/ppt-client:latest &>/dev/null
-sudo docker run --rm -d --name "ppt-client-$player" -p 5900:5900 -v /dev/shm:/dev/shm babakt/ppt-client
+sudo docker pull babakt/ppt-client:cadvise &>/dev/null
+sudo docker run --rm -d --name "ppt-client-$player" -p 5900:5900 -v /dev/shm:/dev/shm babakt/ppt-client:cadvise
 
 sudo docker cp /home/ec2-user/config.json "ppt-client-$player:/home/seluser/ppt/config.json"
 sudo docker exec -d "ppt-client-$player" sudo pm2 start index.js
--- server/init.sh
+++ server/init.sh
@@ -2,13 +2,14 @@
 
 sudo yum -y install docker jq git &>/dev/null
 
+sudo mv /var/lib/docker /dev/
+sudo mkdir /var/lib/docker
+sudo mount --rbind /dev/docker /var/lib/docker
+
 sudo service docker start
 sudo git clone https://github.com/cd-athena/wondershaper.git /home/ec2-user/wondershaper
 
-config=$(cat /home/ec2-user/config.json)
-title=$(echo "$config" | jq -r '.title')
-
-sudo docker pull "babakt/ppt-server-$title:latest" &>/dev/null
-sudo docker run --rm -d --name ppt-server -p 80:80 -v /dev/shm:/dev/shm "babakt/ppt-server-$title"
+sudo docker pull "babakt/ppt-server:cadvise" &>/dev/null
+sudo docker run --rm -d --name ppt-server -p 80:80 -v /dev/shm:/dev/shm "babakt/ppt-server:cadvise"
 
 exit 0
